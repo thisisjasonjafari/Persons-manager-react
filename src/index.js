25626c4b680d4e45ae5d209e0f48c346863e4ae6
thisisjasonjafari <jason.jafari.m@gmail.com>
2020-06-16 00:56:56 -0400

    css styling

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import { render } from 'react-dom';
 import App from './App';
 import './App.css'
 import './index.css'
+// import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+// import '../node_modules/font-awesome/css/font-awesome.min.css'
 
 render((
   <div className="App" >
